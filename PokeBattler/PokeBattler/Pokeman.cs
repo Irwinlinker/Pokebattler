eedeb2e06f53e2318c2382e286be3733e664c373
Robert Marsh <73449694+Irwinlinker@users.noreply.github.com>
2021-07-24 16:55:26 -0600

    Add files via upload

---
+++ PokeBattler/PokeBattler/Pokeman.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokeBattler
+{
+    
+    //pokeman constructor
+    class Pokeman
+    {
+        public string name;
+        public int hp;
+        public string attack1;
+        public string attack2;
+        public int attack1Damage;
+        public int attack2Damage;
+
+        public Pokeman(string aname, int ahp, string aattack1, string aattack2, int aattack1Damage, int aattack2Damage)
+        {
+            name = aname;
+            hp = ahp;
+            attack1 = aattack1;
+            attack2 = aattack2;
+            attack1Damage = aattack1Damage;
+            attack2Damage = aattack2Damage;
+        }
+
+        //return method for attack
+        public int Attack(string aname)
+        {
+            string attack;
+            int attackDamage;
+            string name = aname;
+
+            Random rnd = new Random();
+            int num = rnd.Next(0, 2);
+
+            if (num == 0)
+            {
+                attack = attack1;
+                attackDamage = attack1Damage;
+            }
+            else
+            {
+                attack = attack2;
+                attackDamage = attack2Damage;
+            }
+
+            Console.WriteLine(name + " used " + attack + " and it did " + attackDamage + " damage.");
+            Console.WriteLine("\n");
+
+            return attackDamage;
+        }
+
+    }
+}
---
+++ PokeBattler/PokeBattler/Program.cs
@@ -0,0 +1,58 @@
+﻿//Robert Marsh
+//Program simulates simple Pokemon battle
+
+
+using System;
+
+namespace PokeBattler
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //create pokeman objects
+            Pokeman pokeman1 = new Pokeman("Pikachu", 35, "Charge", "Thunderbolt", 15, 15);
+            Pokeman pokeman2 = new Pokeman("Charmander", 50, "Bite", "Flamethrower", 10, 15);
+
+            //get trainer names
+            Console.Write("Enter Trainer 1 name: ");
+            string trainer1 = Console.ReadLine();
+            Console.Write("Enter Trainer 2 name: ");
+            string trainer2 = Console.ReadLine();
+            Console.WriteLine("\n");
+            //toss out pokeman
+            Console.WriteLine(trainer1 + " chooses " + pokeman1.name + "!");
+            Console.WriteLine("\n");
+            Console.WriteLine(trainer2 + " chooses " + pokeman2.name + "!");
+            Console.WriteLine("\n");
+
+            //while loop for battle
+            while (pokeman1.hp > 0 && pokeman2.hp > 0)
+            {
+
+                //second pokeman attacks first
+                pokeman1.hp -= pokeman2.Attack(pokeman2.name);
+
+                //check for fainted pokeman and declare winner
+                if (pokeman1.hp <= 0)
+                {
+                    //Console.WriteLine(pokeman1.hp + " " + pokeman2.hp);
+                    Console.WriteLine(pokeman1.name + " fainted. " + trainer2 + " and " + pokeman2.name + " win!");
+                    break;
+                }
+
+                //first pokeman attacks
+                pokeman2.hp -= pokeman1.Attack(pokeman1.name);
+
+                //check for fainted pokeman and declare winner
+                if (pokeman2.hp <= 0)
+                {
+                    //Console.WriteLine(pokeman1.hp + " " + pokeman2.hp);
+                    Console.WriteLine(pokeman2.name + " fainted. " + trainer1 + " and " + pokeman1.name + " win!");
+                    break;
+                }
+
+            }
+        }
+    }
+}
